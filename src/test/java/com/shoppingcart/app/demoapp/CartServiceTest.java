2c17044b3ce6d9fb15851f040b40248427048b3d
Elson Renato de Franca <elson.renato.de.franca@gmail.com>
2019-02-09 10:09:12 -0200

    added more unit tests

--- src/test/java/com/shoppingcart/app/demoapp/CartServiceTest.java
+++ src/test/java/com/shoppingcart/app/demoapp/CartServiceTest.java
@@ -1,9 +1,11 @@
 package com.shoppingcart.app.demoapp;
 
 import com.shoppingcart.app.entity.Cart;
+import com.shoppingcart.app.entity.Product;
 import com.shoppingcart.app.exception.CartNotFoundException;
 import com.shoppingcart.app.repository.CartRepository;
 import com.shoppingcart.app.service.ICartService;
+import com.shoppingcart.app.service.IProductService;
 import com.shoppingcart.app.service.impl.CartServiceImpl;
 import org.junit.Rule;
 import org.junit.Test;
@@ -14,6 +16,8 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.Matchers.allOf;
@@ -41,6 +45,9 @@ public class CartServiceTest {
     @Mock
     private CartRepository cartRepository;
 
+    @Mock
+    private IProductService productService;
+
     @InjectMocks
     private ICartService cartService = new CartServiceImpl();
 
@@ -62,17 +69,76 @@ public class CartServiceTest {
     @Test
     public void retrieveCartById_shouldReturnCartNotFoundException_whenCartIdIsNotFound() throws CartNotFoundException {
 
-        Cart mockCar = new Cart();
-        mockCar.setId(1000L);
+        Cart mockCart = createEmptyCart(1000L, true);
 
         //When
-        when(cartRepository.findById(eq(10L))).thenReturn(Optional.of(mockCar));
+        when(cartRepository.findById(eq(10L))).thenReturn(Optional.of(mockCart));
         thrown.expect(CartNotFoundException.class);
 
         Cart newCart = cartService.retrieveCartById(1L);
 
         //Then
         assertThat(newCart, is(nullValue()));
+    }
+
+    @Test
+    public void retrieveCartById_shouldReturnCartEmpty_whenNoProductWasAddedToTheCart() throws CartNotFoundException {
+
+        Cart mockCart = createEmptyCart(1000L, true);
+        Long expectedId = 1000L;
+
+        //When
+        when(cartRepository.findById(eq(expectedId))).thenReturn(Optional.of(mockCart));
+        Cart newCart = cartService.retrieveCartById(expectedId);
+
+        verify(cartRepository, times(1)).findById(eq(expectedId));
+
+        //Then
+        assertThat(newCart, is(notNullValue()));
+        assertThat(newCart, hasProperty("products", is(nullValue())));
+    }
+
+    @Test
+    public void retrieveCartById_shouldReturnWithProducts_whenProductsWereAddedToTheCart() throws CartNotFoundException {
+
+        Cart mockCart = createEmptyCart(1000L, false);
+        Long expectedId = 1000L;
+
+        //When
+        when(cartRepository.findById(eq(expectedId))).thenReturn(Optional.of(mockCart));
+        Cart newCart = cartService.retrieveCartById(expectedId);
+
+        verify(cartRepository, times(1)).findById(eq(expectedId));
+
+        //Then
+        assertThat(newCart, is(notNullValue()));
+        assertThat(newCart, hasProperty("products", is(notNullValue())));
+    }
+
+    private Cart createEmptyCart(Long cartId, boolean emptyCart){
+
+        Cart newCart = new Cart();
+
+        if(!emptyCart){
+            List<Product> products = createProductList();
+            newCart.setId(cartId);
+            newCart.setProducts(products);
+        }
+        return newCart;
+    }
+
+    private List<Product> createProductList(){
+        Product product = new Product();
+        product.setCost(20.00);
+        product.setDescription("Product 1");
+        product.setId(1L);
+        product.setProductId(10L);
+        product.setName("Nike shoes");
+        List<Product> products = new ArrayList<>();
+        products.add(product);
+
+        return products;
+
     }
 
 
